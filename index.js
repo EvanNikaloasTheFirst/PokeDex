13ed3e8f24637b48971a300b572529b2ff609797
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2022-09-02 11:01:57 +0100

    completed project

--- index.js
+++ index.js
@@ -44,25 +44,11 @@ submitBtn.addEventListener("click", function obtainPokemon(){
             var tmOne = data.moves[0].move.name
             var tmTwo = data.moves[1].move.name
             var tmThree = data.moves[2].move.name
-            var tmFour = data.moves[3].move.name
-            var tmFive = data.moves[4].move.name
-            var tmSix = data.moves[5].move.name
-            var tmSeven = data.moves[6].move.name
-            var tmEight = data.moves[7].move.name
-            var tmNine = data.moves[8].move.name
+            
 
             var moveOne = document.querySelector('.MoveOne') .innerHTML = tmOne
             var moveTwo = document.querySelector('.MoveTwo') .innerHTML = tmTwo
             var moveThree = document.querySelector('.MoveThree') .innerHTML = tmThree;
-            var moveFour = document.querySelector('.MoveFour') .innerHTML = tmFour
-            var moveFive = document.querySelector('.MoveFive') .innerHTML = tmFive
-            var moveSix = document.querySelector('.MoveSix') .innerHTML = tmSix
-            var moveSeven = document.querySelector('.MoveSeven') .innerHTML = tmSeven
-            var moveEight = document.querySelector('.MoveEight') .innerHTML = tmEight
-
-
-console.log(tmNine)
-            var pokemonTypetwo = data.types[1].type.name
             
     
     // Attaches the data from the JSON file into the front end aspect of the application
@@ -71,7 +57,6 @@ console.log(tmNine)
         var imageOfPokemon = document.querySelector('#pokemonImage').src = pokemonImage
         var imageOfPokemon2 = document.querySelector('#pokemonImage2').src = pokeImage2
         var pokemonFirstType = document.querySelector('.pokeTypeOne').innerHTML = pokemonTypeone.toUpperCase() 
-        var pokemonSecondType = document.querySelector('.pokeTypeTwo').innerHTML = pokemonTypetwo;
         var healthPoints = document.querySelector('.HP').innerHTML = "HP: " + hp
         var attackPoints = document.querySelector('.ATK').innerHTML = "ATK: " + atk
         var defencePoints = document.querySelector('.DEF').innerHTML = "DEF: " +def
